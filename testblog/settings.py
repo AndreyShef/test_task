ebbffb5c55b451e81e30e05c7691221958828362
AndreyShekhovtsov <shekhovtsovandrey@mail.ru>
2021-09-01 21:25:38 +0300

    изменение settings, gitignore

--- testblog/settings.py
+++ testblog/settings.py
@@ -9,18 +9,20 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+from .settings_new import *
+# from pathlib import Path
 
-from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--_1l_%5(#w8-s@sobq_%(g6%of6eq%nc)h%b3u9)j!n2s)n#kb'
+SECRET_KEY = key_new
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,12 +76,7 @@ WSGI_APPLICATION = 'testblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = db_new
 
 
 # Password validation
